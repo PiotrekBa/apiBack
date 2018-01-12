3334d5843c35294a23d7ca8ef72a9b3c1f66f815
Piotr <p_bartoszak@wp.pl>
2018-01-12 15:31:23 +0100

    Add MemoryBookService

--- src/main/java/pl/coderslab/apiBack/app/AppConfig.java
+++ src/main/java/pl/coderslab/apiBack/app/AppConfig.java
@@ -7,7 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "pl.coderslab")
+@ComponentScan(basePackages = {"pl.coderslab.apiBack.model", 
+		"pl.coderslab.apiBack.controller"})
 public class AppConfig extends WebMvcConfigurerAdapter {
 
 }
--- src/main/java/pl/coderslab/apiBack/controller/BookController.java
+++ src/main/java/pl/coderslab/apiBack/controller/BookController.java
@@ -20,4 +20,6 @@ public class BookController {
 				"programming");
 	}
 	
+	
+	
 }
